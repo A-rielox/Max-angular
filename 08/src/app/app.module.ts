c2a82e38cec662e8b4c0739d296632eef803f56a
A-rielox <arielox.ag@gmail.com>
2022-09-22 16:44:29 -0300

    hasta 147

--- 08/src/app/app.module.ts
+++ 08/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -10,25 +9,12 @@ import { ServersComponent } from './servers/servers.component';
 import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 import { ServersService } from './servers/servers.service';
-
-const appRoutes: Routes = [
-   { path: '', component: HomeComponent },
-   {
-      path: 'users',
-      component: UsersComponent,
-      children: [{ path: ':id/:name', component: UserComponent }],
-   },
-   {
-      path: 'servers',
-      component: ServersComponent,
-      children: [
-         { path: ':id', component: ServerComponent },
-         { path: ':id/edit', component: EditServerComponent },
-      ],
-   },
-];
+import { AppRoutingModule } from './app-routing.module';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth-guard.service';
 
 @NgModule({
    declarations: [
@@ -39,9 +25,10 @@ const appRoutes: Routes = [
       UserComponent,
       EditServerComponent,
       ServerComponent,
+      PageNotFoundComponent,
    ], // RouterModule para router
-   imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
-   providers: [ServersService],
+   imports: [BrowserModule, FormsModule, AppRoutingModule],
+   providers: [ServersService, AuthService, AuthGuard],
    bootstrap: [AppComponent],
 })
 export class AppModule {}
