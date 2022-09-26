b3ccfe46d519ebc42c9158c3a7e1c22f263bc69e
A-rielox <arielox.ag@gmail.com>
2022-09-26 15:39:17 -0500

    en forms, hasta 199

--- 16-forms/src/app/app.component.ts
+++ 16-forms/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,6 +7,13 @@ import { NgForm } from '@angular/forms';
    styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+   @ViewChild('f') signupForm: NgForm;
+   // este valor es el value="" en la option.
+   // [ngModel]="defaultQuestion" al cambiar el valor de la opcion NO se actualiza el valor de la variable, para eso se ocupa 2-way-binding
+   defaultQuestion = 'pet';
+   answer = '';
+   genders = ['male', 'female'];
+
    suggestUserName() {
       const suggestedName = 'Superuser';
    }
@@ -14,6 +21,24 @@ export class AppComponent {
    onSubmit(form: NgForm) {
       // console.log(form.value);
       // {username: 'Arielox', email: 'test1@test1.com', secret: 'teacher'}
-      console.log(form);
+      console.log(form.value);
    }
+
+   // ngModelGroup="userData"
+   // en el tamplate me agrupa los input
+   // value: Object
+   //    questionAnswer: "holi hola"
+   //    secret: "pet"
+   //    userData: {username: 'Arielox', email: 'test1@test1.com'}
+
+   // en el tamplate
+   // ngModel para especificar q es un form-control
+   // [ngModel] para poner valor xdefault ( con proprety binding )
+   // [(ngModel)] para poner para q tambien se actualize el valor de la variable en el archivo .ts
+
+   // puedo NO pasar la referencia a la form en (ngSubmit)="onSubmit()" y acceder a la form mediante ViewChild
+   /* onSubmit() {
+      
+      console.log(this.signupForm.value);
+   } */
 }
