f2e52d0ed4eacf5092738a4e53b8287bc15f7cf3
A-rielox <arielox.ag@gmail.com>
2022-09-27 13:44:42 -0500

    hasta 113

--- 16-forms -- Reactive approach/src/app/app.component.ts
+++ 16-forms -- Reactive approach/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
    selector: 'app-root',
@@ -9,13 +9,25 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class AppComponent implements OnInit {
    genders = ['male', 'female'];
    signupForm: FormGroup;
+   forbiddenUsernames = ['Chris', 'Anna'];
 
    ngOnInit() {
       this.signupForm = new FormGroup({
          // el 1er arg es el valor default, podria ser "Default User"
-         username: new FormControl(null),
-         email: new FormControl(null),
+         userData: new FormGroup({
+            // username: new FormControl(null, Validators.required),
+            username: new FormControl(null, [
+               Validators.required,
+               this.forbiddenNames.bind(this),
+            ]),
+            email: new FormControl(null, [
+               Validators.required,
+               Validators.email,
+            ]),
+         }),
+
          gender: new FormControl('male'),
+         hobbies: new FormArray([]),
       });
    }
 
@@ -23,5 +35,20 @@ export class AppComponent implements OnInit {
       // en responsive approach NO necesito la reference ( #f ) para acceder a la forma
       console.log(this.signupForm.value);
       // {username: 'Arielox', email: 'test1@test1.com', gender: 'female'}
+   }
+
+   onAddHobbie() {
+      const control = new FormControl(null, Validators.required);
+      (<FormArray>this.signupForm.get('hobbies')).push(control);
+   }
+
+   // el custom validator
+   forbiddenNames(control: FormControl): { [s: string]: boolean } {
+      if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+         return { nameIsForbidden: true };
+      }
+
+      // validator superado
+      return null;
    }
 }
