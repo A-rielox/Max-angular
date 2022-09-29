ff5d7bed546fd2eb9a847b4b720d4eaefdff0a20
A-rielox <arielox.ag@gmail.com>
2022-09-29 00:52:31 -0500

    listo http request teoria

--- 18-Making Http Requests/src/app/app.module.ts
+++ 18-Making Http Requests/src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { AuthInterceptorService } from './auth-interceptor.service';
+import { LoggingInterceptorService } from './logging-interceptor.service';
 
 @NgModule({
    declarations: [AppComponent],
    imports: [BrowserModule, FormsModule, HttpClientModule],
-   providers: [],
+   providers: [
+      {
+         provide: HTTP_INTERCEPTORS,
+         useClass: AuthInterceptorService,
+         multi: true,
+      },
+      {
+         provide: HTTP_INTERCEPTORS,
+         useClass: LoggingInterceptorService,
+         multi: true,
+      },
+   ],
    bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+/*  PARA PODER OCUPAR EL INTERCEPTOR, SE EJECUTAN EN EL ORDEN Q LOS PONGO ACA
+
+{
+   provide: HTTP_INTERCEPTORS,
+   useClass: AuthInterceptorService,
+   multi: true,
+}
+*/
 
 // para ocupar [(ngModel)] en los .html tengo q importar import { FormsModule } from '@angular/forms' en app.module y ponerlo en el imports
---
+++ 18-Making Http Requests/src/app/auth-interceptor.service.ts
@@ -0,0 +1,37 @@
+import {
+   HttpHandler,
+   HttpInterceptor,
+   HttpRequest,
+} from '@angular/common/http';
+import { tap } from 'rxjs';
+
+export class AuthInterceptorService implements HttpInterceptor {
+   intercept(req: HttpRequest<any>, next: HttpHandler) {
+      const modifiedRequest = req.clone({
+         headers: req.headers.append('Auth', 'XDXDXD'),
+      });
+
+      return next.handle(modifiedRequest);
+   }
+}
+/*  .pipe PARA MODIFICAR LA RESPUESTA QUE LLEGA ( PUEDO NO PONERLA )
+
+*/
+
+/*  PARA DISCRIMINAR EN QUE REQUEST SE MANDE 
+puero poner dentro de intercept un if con 
+if(req.url === http.....)...
+
+*/
+
+/*  
+en app.module PARA PODER OCUPAR EL INTERCEPTOR
+
+providers: [
+   {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true,
+   },
+],
+*/
