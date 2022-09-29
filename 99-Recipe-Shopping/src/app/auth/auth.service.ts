d1fb2f09343aa56ea3aaf6b8b04878fdeb0b5ff7
A-rielox <arielox.ag@gmail.com>
2022-09-29 18:30:26 -0500

    hasta 298

--- 99-Recipe-Shopping/src/app/auth/auth.service.ts
+++ 99-Recipe-Shopping/src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
 
 interface AuthResponseData {
    kind: string;
@@ -18,11 +19,29 @@ export class AuthService {
    constructor(private http: HttpClient) {}
 
    signup(email: string, password: string) {
-      return this.http.post<AuthResponseData>(this.fbUrl, {
-         email: email,
-         password: password,
-         returnSecureToken: true,
-      });
+      return this.http
+         .post<AuthResponseData>(this.fbUrl, {
+            email: email,
+            password: password,
+            returnSecureToken: true,
+         })
+         .pipe(
+            catchError((errorRes) => {
+               let errorMessage = 'An unexpected error occured.';
+               console.log(errorRes);
+
+               if (!errorRes.error || !errorRes.error.error) {
+                  return throwError(errorMessage);
+               }
+
+               switch (errorRes.error.error.message) {
+                  case 'EMAIL_EXISTS':
+                     errorMessage = 'This email already exists';
+               }
+
+               return throwError(errorMessage);
+            })
+         );
    }
 }
 
